3d4ef3178c80eaae8a053f28b030e11409c4722f
prajith <rprajith7@gmail.com>
2023-07-31 17:19:36 +0530

    Test

--- app.js
+++ app.js
@@ -1,65 +1,83 @@
-let objData={};
+let objData=JSON.parse(localStorage.getItem('Data')) || {};
 let addTask = document.getElementById("submitbtn")
 let titlevalue = document.getElementById("text-box")
 let descriptionVal = document.getElementById("description")
-console.log(titlevalue)
-
-addTask.addEventListener("click",()=>{
-    if(objData[titlevalue.value]===undefined)
-        objData[titlevalue.value]={title:titlevalue.value, notes:descriptionVal.value}
-        console.log(objData)
+let displayTask = document.getElementById("container-2")
+let key =Number(Object.keys(objData).pop())+1 || 0,temp=0;
+console.log(objData,key)
+showTask();
+addTask.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(titlevalue.value!==" " && descriptionVal.value!==" "){
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        console.log(addTask.value)
+        if(addTask.value==="submit"){
+            console.log(key)
+            objData[key]={title:titlevalue.value, notes:descriptionVal.value ,created:time,updated:time}
+            }
+        else{
+            objData[temp]={title:titlevalue.value, notes:descriptionVal.value ,created:time,updated:time}
+            objData[temp]['title']=titlevalue.value
+            objData[temp]['notes']=descriptionVal.value
+            objData[temp]['updated']=time;
+        }
+        key+=1;
+         titlevalue.value=" "
+        descriptionVal.value=" "
+        addTask.value="submit"
+        localStorage.setItem("Data",JSON.stringify(objData))
+        showTask(); 
+    }
+})
+function showTask(){
+    displayTask.innerHTML=" "
+    for(let val in objData){
+    
+        console.log(addTask)
+        let editBtn = document.createElement("button")
+        editBtn.classList.add("edit")
+        editBtn.innerText="edit"
+        let dltBtn = document.createElement("button")
+        dltBtn.classList.add("dlt")
+        dltBtn.innerText="delete"
+        let titlediv1 = document.createElement("div")
+        let showTitle=document.createElement("p")
+        showTitle.style.cursor="pointer";
+        let showContent = document.createElement("p")
+        let currentTime = document.createElement("p")
+        let updateTime = document.createElement("p")
+        showContent.setAttribute("class","hidden")
+        showContent.innerText = objData[val].notes;
+        showTitle.innerText = objData[val].title;
+        currentTime.innerText="current time " + objData[val].created;
+        updateTime .innerText="update time " +objData[val].updated;
+        showTitle.addEventListener("click",(e)=>{
+            e.preventDefault();
+            showContent.classList.toggle("hidden")
+        })
+        titlediv1.appendChild(showTitle)
+        displayTask.appendChild(titlediv1)
+        titlediv1.appendChild(editBtn)
+        titlediv1.appendChild(dltBtn)
+        titlediv1.appendChild(showContent)
+        titlediv1.appendChild(currentTime)
+        titlediv1.appendChild(updateTime)
+    dltBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        delete objData[val];
+        localStorage.setItem("Data",JSON.stringify(objData))
         showTask();
-
-        function showTask(){
-            let displayTask = document.getElementById("container-2")
-console.log(addTask)
-let editBtn = document.createElement("button")
-editBtn.classList.add("edit")
-editBtn.innerText="edit"
-let dltBtn = document.createElement("button")
-dltBtn.classList.add("dlt")
-dltBtn.innerText="delete"
-let titlediv1 = document.createElement("div")
-let titlediv2 = document.createElement("div")
-let showTitle=document.createElement("p")
-showTitle.innerText = titlevalue.value;
-    titlediv2.appendChild(titlediv1)
-    titlediv1.appendChild(showTitle)
-    displayTask.appendChild(titlediv2)
-    titlediv2.appendChild(editBtn)
-    titlediv2.appendChild(dltBtn)
-    console.log(displayTask)
-        }})
-//    if(titlevalue.value && descriptionVal.value){
-    
-    
-    // let table = document.createElement('table');
-    // let  rows = document.createElement('tr');
-    // let td1= document.createElement("td");
-    // let td2= document.createElement("td");
-    // let td3= document.createElement("td");
-
-    
-    //     displayTask.appendChild(table)
-    //     rows.appendChild(td1)
-    //     rows.appendChild(td2)
-    //     rows.appendChild(td3)
-    //     td1.innerText=titlevalue.value;
-    //     td2.appendChild(editBtn)
-    //     td3.appendChild(dltBtn)
-    //     table.appendChild(rows)
-//     titlevalue.value=" " ;
-//     descriptionVal.value=" ";
-//     editBtn.addEventListener("click",()=>{
-
-//     })
-//    }
-   
-
-   
-
-// })
-// dltBtn.addEventListener("click",()=>{
-//     displayTask.remove(titlediv1)
-//    })
-
+    })
+    editBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        console.log("hello")
+        displayTask.removeChild(titlediv1)
+        titlevalue.value=objData[val].title
+        descriptionVal.value=objData[val].notes
+        addTask.value="update";
+        temp=val;
+        delete objData[val];
+    })
+}
+}
